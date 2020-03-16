aa4c7ddcc07cb655d81ea0f9e54d27f086c81782
dipeshkoirala <dipeshkoirala@gmail.com>
2020-03-15 20:24:59 -0400

    stretch remaining

--- index.js
+++ index.js
@@ -306,7 +306,7 @@ function firstNamesAllCaps(/* CODE HERE */ runners) {
  * @returns an array containing only the runners that use the given `tShirtSize`.
  * The runners in the array appear in the same order they appear in the `runners` array.
  */
-function getRunnersByTShirtSize(/* CODE HERE */) {
+function getRunnersByTShirtSize(/* CODE HERE */ runners, tShirtSize) {
   /* CODE HERE */
 }
 
